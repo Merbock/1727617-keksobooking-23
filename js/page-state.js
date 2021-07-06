7a12212c6e81c346803dbd569fb6c5487849bb3c
Merbock <merbock7@gmail.com>
2021-07-06 23:56:55 +0300

    Добавляет валидацию формы

---
+++ js/page-state.js
@@ -0,0 +1,22 @@
+const adForm = document.querySelector('.ad-form');
+const mapFilter = document.querySelector('.map__filters');
+
+const toggleElements = (form, flag) => {
+  [...form.elements].forEach((element) => element.disabled = flag);
+};
+
+const disablePage = () => {
+  adForm.classList.add('ad-form--disabled');
+  mapFilter.classList.add('map__filters--disabled');
+  toggleElements(adForm, true);
+  toggleElements(mapFilter, true);
+};
+
+const enablePage = () => {
+  adForm.classList.remove('ad-form--disabled');
+  mapFilter.classList.remove('map__filters--disabled');
+  toggleElements(adForm, false);
+  toggleElements(mapFilter, false);
+};
+
+export {disablePage, enablePage};
